bade2548daa3b6913c74e6907c4415b6255f6bb0
Валерий Иванов <va.ivanov@cian.ru>
2022-05-17 21:44:24 +0300

    init

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let inNewTab: boolean;
+	export let href: string;
+</script>
+
+<a
+	target={inNewTab ? '_blank' : ''}
+	rel={inNewTab ? 'noopener noreferrer' : ''}
+	class="text-rose-600 dark:text-rose-700 hover:underline underline-offset-2"
+	{href}
+>
+	<slot />
+</a>
---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<script context="module">
+  export const load = async () => {
+		const ReadMeFile = await import('../../README.md');
+    const ReadMe = ReadMeFile.default;
+
+		return {
+			props: {
+				ReadMe
+			}
+		}
+	}
+</script>
+
+<script>
+  export let ReadMe
+</script>
+
+
+<svelte:head>
+	<title>SvelteKit Static Blog Starter</title>
+</svelte:head>
+
+
+<svelte:component this={ReadMe} />
+<!-- This is the README.md file in the root of the repo. It serves double duty as the homepage's content. If you'd rather use your own HTML and/or Svelte, you can delete/modify everything above this line. -->
